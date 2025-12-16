53bfa90c54113c5625c0d63eae77850998181104
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2025-12-16 18:30:10 +0100

    [3.0.0] Added --ping argument

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Introduction
+
 The `first-response-time.js` script can be used to collect the first response time also known as the time to first request.
 
 The script executes a command and makes requests to a specific URL in 100 ms intervals until it receives the 200 HTTP status code with a response body.
@@ -6,14 +7,27 @@ The script executes a command and makes requests to a specific URL in 100 ms int
 The script finally prints out the duration between the command execution and the time it received the 200 status.
 
 # Installation
+
 1. Install Node.js with npm
 1. Run `npm i -g first-response-time`
 
 # Usage
 
-```java
-first-response-time "<COMMAND>" <URL>
+```sh
+first-response-time "<COMMAND>" <URL> [--ping <INTERVAL_MS>]
 ```
 
 `<COMMAND>` a correctly escaped system command.
 `<URL>` request url.
+
+(optional) --ping <INTERVAL_MS> (default: 100 milliseconds)
+
+# Examples
+
+```sh
+first-response-time "java -jar uber-jar.jar" http://localhost:8080/hello
+```
+
+```sh
+first-response-time "node script.js" http://localhost:8080/hello --ping 20
+```
--- first-response-time.js
+++ first-response-time.js
@@ -1,43 +1,68 @@
 #!/usr/bin/env node
 
-const exec = require("child_process").exec;
-const request = require("request");
+import { exec } from "node:child_process";
 
-const pingIntervalMs = 100;
+import request from "request-compose";
+import kill from "tree-kill";
+
+let pingIntervalMs = 100;
 
 const args = process.argv.slice(2);
 if (args.length == 0) {
   console.log('Usage: first-response-time "<COMMAND>" <URL>');
+  console.log("   (optional) --ping <INTERVAL_MS> (default: 100 milliseconds)");
   process.exit(-1);
 }
 
 const command = args.shift();
 const requestUrl = args.shift();
 
+const ping = args.shift();
+if (ping === "--ping") {
+  const interval = args.shift();
+  if (interval) {
+    const parsedInterval = parseInt(interval, 10);
+    if (!isNaN(parsedInterval) && parsedInterval > 0) {
+      pingIntervalMs = parsedInterval;
+    }
+  }
+}
+
 const proc = exec(command, { timeout: 60000 }, (error) => {
   if (error) {
-    console.error(error);
-    proc.kill();
+    console.error("Error: " + error.message);
+    kill(proc.pid);
     process.exit(-1);
   }
 });
 
 const startTime = new Date().getTime();
-const intervalHandle = setInterval(() => {
-  request(requestUrl, (error, response, body) => {
-    if (!error && response && response.statusCode === 200 && body) {
-      const time = new Date().getTime() - startTime;
-      console.log(time + " ms");
-      clearInterval(intervalHandle);
-      proc.kill();
-      process.exit(0);
-    } else {
-      if (!error || error.code !== "ECONNREFUSED") {
-        console.log(error ? console.log(JSON.stringify(error)) : "Received HTTP status code: " + response.statusCode);
-        proc.kill();
-        process.exit(-1);
-      }
+const intervalHandle = setInterval(async () => {
+  let error = false;
+  let responseReceived = false;
+  try {
+    const { res } = await request.client({
+      url: requestUrl,
+    });
+    responseReceived = true;
+    if (res.statusCode != 200) {
+      console.log("Received status code: " + res.statusCode);
+      error = true;
     }
+  } catch (err) {
+    if (err?.code !== "ECONNREFUSED") {
+      error = true;
+      responseReceived = true;
+      console.error("Error: " + err.message);
+    }
+  } finally {
+    if (!responseReceived) {
+      return;
+    }
+    const time = new Date().getTime() - startTime;
+    console.log(`Received response in ${time} milliseconds`);
+    clearInterval(intervalHandle);
+    kill(proc.pid);
+    process.exit(error ? -1 : 0);
   }
-  );
 }, pingIntervalMs);
